ad600f3198309e322d2b79b8401e89cd64cf0a30
Dhruv Singh <dhruvsingh7077@gmail.com>
2026-03-30 20:44:35 +0530

    +controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,16 @@
 import express from "express";
-import dotenv from "dotenv";
+import {ENV} from "./lib/env.js";
+
+
 
 dotenv.config();
 const app =express();
 
-console.log(process.env.PORT)
+console.log(ENV.PORT)
+console.log(ENV.DB_URL)
 app.get("/", (req,res)=>{
     res.status(200).json({msg: "success from backend "})
 });
 
-app.listen(3000, ()=> console.log("Server is running on port 3000"));
+app.listen(ENV.PORT, ()=> console.log("Server is running on port:",ENV.PORT));
    
