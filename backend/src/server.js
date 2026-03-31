0fcc789a59f2e6a1737e56dc2d6853446cd2d143
Dhruv Singh <dhruvsingh7077@gmail.com>
2026-03-31 21:14:44 +0530

    +production env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,23 @@
 import express from "express";
+import path from "path";
 import {ENV} from "./lib/env.js";
 
-
-
-dotenv.config();
 const app =express();
 
-console.log(ENV.PORT)
-console.log(ENV.DB_URL)
-app.get("/", (req,res)=>{
+const __dirname = path.resolve();
+
+app.get("/api", (req,res)=>{
     res.status(200).json({msg: "success from backend "})
 });
+
+// make our app ready fro deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=> {
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(ENV.PORT, ()=> console.log("Server is running on port:",ENV.PORT));
    
