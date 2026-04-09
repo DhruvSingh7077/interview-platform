55d5ed52d06a3d25b379045cae3a0a3f5e82f567
Dhruv Singh <dhruvsingh7077@gmail.com>
2026-04-09 21:07:55 +0530

    +steam service updated

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,5 +1,19 @@
 import {StreamChat} from "stream-chat"
 import {ENV }from "./env.js"
 
-const apiKey = ENV.STREAM_API_KEY
-const apiSecret = ENV.STREAM_API_SECRET
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    console.log("Streamapi key is missing");
+}
+export const chatClient = StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser = async(userData) => {
+    try {
+        await chatClient.upsertUsers([userData])
+        return userData
+    } catch (error) {
+        console.log("Error upserting stream user:",error)
+    }
+}
